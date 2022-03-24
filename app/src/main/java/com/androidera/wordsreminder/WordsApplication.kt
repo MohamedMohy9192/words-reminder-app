274daf2deea6eec5cfe71d8bd8736f51bfc9dad3
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-03-24 17:35:13 +0200

    Add callback on creating database to populate it

--- app/src/main/java/com/androidera/wordsreminder/WordsApplication.kt
+++ app/src/main/java/com/androidera/wordsreminder/WordsApplication.kt
@@ -3,8 +3,23 @@ package com.androidera.wordsreminder
 import android.app.Application
 import com.androidera.wordsreminder.data.WordRoomDatabase
 import com.androidera.wordsreminder.repository.WordRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 
 class WordsApplication : Application() {
-    val database: WordRoomDatabase by lazy { WordRoomDatabase.getDatabase(this) }
+    // The default behavior if an child job thrown an exception, it is ends up causing the parent
+    // job that launch that child to fail and then all the jobs that are currently active within
+    // that parent to fail a one way to get around this is by surrounding the job that
+    // could throw and exception with try catch block that will prevent the parent job to fail
+    // and prevent other jobs within the parent to fail or we can use a supervisor job
+
+    // Children of a supervisor job can fail independently of each other.
+    // A failure or cancellation of a child does not cause the supervisor job to fail and does
+    // not affect its other children
+
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
+    val database: WordRoomDatabase by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository: WordRepository by lazy { WordRepository(database.wordDao()) }
 }
--- app/src/main/java/com/androidera/wordsreminder/data/WordRoomDatabase.kt
+++ app/src/main/java/com/androidera/wordsreminder/data/WordRoomDatabase.kt
@@ -1,25 +1,51 @@
 package com.androidera.wordsreminder.data
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
 
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { wordRoomDatabase ->
+                Log.i("WordRoomDatabase", Thread.currentThread().name)
+                scope.launch { populateDatabase(wordRoomDatabase.wordDao()) }
+            }
+        }
+
+        private suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+            wordDao.deleteAll()
+            // Add sample words.
+            wordDao.insert(Word(word = "Hello"))
+            wordDao.insert(Word(word = "Word"))
+        }
+    }
+
     companion object {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_databaase"
-                ).build()
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
 
                 INSTANCE = instance
 
